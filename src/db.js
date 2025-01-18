96f7bbf47ff07b25bb773d436051836c579fc928
ivanaplese <ivanaplese123@gmail.com>
2025-01-18 15:33:23 +0100

    i sad radi sve s mongom (:thumbsup:)

--- src/db.js
+++ src/db.js
@@ -1,10 +1,7 @@
 import { MongoClient } from "mongodb";
-
 const connectionString =
-    "mongodb+srv://test:test@clustertest.ndyns6h.mongodb.net/?retryWrites=true&w=majority";
-
+    "mongodb+srv://test:test@cluster0.vinwyba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 let db;
-
 try {
     console.log("Trying to establish connection to MongoDB...");
     const client = new MongoClient(connectionString);
@@ -14,7 +11,6 @@ try {
 } catch (error) {
     console.error("Error connecting to MongoDB:", error);
 }
-
 export default db;
 
 
